327051a280fe60ad9d463131f248bc35dae6a8f0
José Ricardo <Jos.Ricardo@PegPese.com>
2021-11-10 15:08:04 -0300

    layout messages ajustado

--- Routers/router.js
+++ Routers/router.js
@@ -33,7 +33,7 @@ export class Routers {
                 case 'message':
                     result = new MessagePage;
                     document.getElementById('StylePages').setAttribute('href',"./Pages/Message/message.css")
-                    local.insertAdjacentHTML("beforeend", result.main());
+                    local.insertAdjacentHTML("beforeend", await result.main());
                     break;
             }
             return result;
