a9d081ebabddea928d3e8c7367c5c02f7d07d075
Hygor Bueno <hygor_knd.x1@hotmail.com>
2021-12-03 08:40:42 -0300

    criada caixa de edição e adição  das perguntas

--- Pages/Checklist/CreateChecklist/settingChecklist.js
+++ Pages/Checklist/CreateChecklist/settingChecklist.js
@@ -1,3 +1,10 @@
-export class SettingChecklist{
+import { TemplateChecklist } from "../../../Components/templateChecklist.js";
 
+
+export class SettingChecklist{
+    setting(){
+        let templateChecklist = new TemplateChecklist;
+
+        templateChecklist.settingButton();
+    }
 }
