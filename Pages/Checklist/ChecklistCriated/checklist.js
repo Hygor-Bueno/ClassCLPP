abc2a66bf0ddd398f54e3b91a8cca747c7e98c96
bethuel <bethuel.pg@gmail.com>
2021-12-14 10:07:33 -0300

    correct

--- Pages/Checklist/ChecklistCriated/checklist.js
+++ Pages/Checklist/ChecklistCriated/checklist.js
@@ -2,9 +2,7 @@ export class ChecklistCreatedPage {
   main() {
     document.getElementById("message").setAttribute("style", "display:none");
     let response = `
-        <h1>Pagina dos Checklists Criados</h1>        
-        <h1>Pagina dos Checklists Criados</h1>        
-        <h1>Pagina dos Checklists Criados</h1>        
+        <h1>Pagina dos Checklists Criados</h1>            
         `;
     return response;
   }
