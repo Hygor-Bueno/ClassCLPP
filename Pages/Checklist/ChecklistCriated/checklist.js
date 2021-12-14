d508006083651f8d9ab941b59d09918f67f1dc0b
bethuel <bethuel.pg@gmail.com>
2021-12-14 09:13:16 -0300

    new branch

--- Pages/Checklist/ChecklistCriated/checklist.js
+++ Pages/Checklist/ChecklistCriated/checklist.js
@@ -1,11 +1,10 @@
-export class ChecklistCreatedPage{
-    
-    main(){
-        document.getElementById('message').setAttribute('style', 'display:none')
-        let response = 
-        `
+export class ChecklistCreatedPage {
+  main() {
+    document.getElementById("message").setAttribute("style", "display:none");
+    let response = `
         <h1>Pagina dos Checklists Criados</h1>        
-        `
-        return response;
-    }
+        <h1>Pagina dos Checklists Criados</h1>        
+        `;
+    return response;
+  }
 }
