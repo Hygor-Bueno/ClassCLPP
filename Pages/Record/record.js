b6c6257a10a32e1fafcdbbf39a2e24d8e0974c19
bethuel <bethuel.pg@gmail.com>
2022-01-18 15:54:55 -0300

    funcionalidade do btn da filtragem checklist arrumada

--- Pages/Record/record.js
+++ Pages/Record/record.js
@@ -16,7 +16,7 @@ export class RecordPage extends SettingRecord {
                     <div class="titleBlock">                
                         <div class="titleFilter">     
                             <h1>Checklist</h1>  
-                            <button class="imgButton openCloseBlock" data-block="checklistBlock" type="button"><img src="./assets/images/setaCima.svg"/></button>               
+                            <button class="imgButton openCloseBlock openCloseBtnCss" data-block="checklistBlock" type="button"></button>               
                         </div>                   
                         <div class="bodyChecklist" style="display:none" id="checklistBlock">
                             <div class="questionChecklist">
@@ -37,26 +37,18 @@ export class RecordPage extends SettingRecord {
                             </div>
                             <div class="questionChecklist">
                                 <label>Validade</label>
-                                <select>
-                                    <option>Opções</option>
-                                    <option>Uva</option>
-                                    <option>Arroz</option>
-                                </select>
+                                <input type="date"></input>
                             </div>
                             <div class="questionChecklist">
                                 <label>Validade</label>
-                                <select>
-                                    <option>Opções</option>
-                                    <option>Uva</option>
-                                    <option>Arroz</option>
-                                </select>
+                                <input type="date"></input>
                             </div>
                         </div>
                     </div>
                     <div class="titleBlock">                
                         <div class="titleFilter">     
                             <h1>Grupos Criados</h1>  
-                            <button class="imgButton openCloseBlock" data-block="grupsCreatedBlock" type="button"><img src="./assets/images/setaCima.svg"/></button>               
+                            <button class="imgButton openCloseBlock openCloseBtnCss" data-block="grupsCreatedBlock" type="button"></button>               
                         </div>
                         <div id="grupsCreatedBlock" class="bodyChecklist" style="display:none">
                             <div class="questionChecklist">
@@ -71,14 +63,14 @@ export class RecordPage extends SettingRecord {
                     <div class="titleBlock">                
                         <div class="titleFilter">     
                             <h1>Tipo de relatório</h1>  
-                            <button class="imgButton openCloseBlock" data-block="recordBlock" type="button"><img src="./assets/images/setaCima.svg"/></button>               
+                            <button class="imgButton openCloseBlock openCloseBtnCss" data-block="recordBlock" type="button"></button>               
                         </div>
                         <div id="recordBlock" class="bodyChecklist" style="display:none">
                             <div class="questionChecklist" >
                                 <select>
-                                    <option>Opções</option>
-                                    <option>Uva</option>
-                                    <option>Arroz</option>
+                                    <option value="opt">Opções</option>
+                                    <option value="uva">Uva</option>
+                                    <option value="arroz">Arroz</option>
                                 </select>
                             </div>
                         </div>  
@@ -86,7 +78,7 @@ export class RecordPage extends SettingRecord {
                     <div class="titleBlock">                
                         <div class="titleFilter">     
                             <h1>Unidades Grupos Criados</h1>  
-                            <button class="imgButton openCloseBlock" data-block="unidGroupBlock" type="button"><img src="./assets/images/setaCima.svg"/></button>               
+                            <button class="imgButton openCloseBlock openCloseBtnCss" data-block="unidGroupBlock" type="button"></button>               
                         </div>
                         <div id="unidGroupBlock" class="bodyChecklist" style="display:none">
                             <div class="questionChecklist">
@@ -101,7 +93,7 @@ export class RecordPage extends SettingRecord {
                     <div class="titleBlock">                
                         <div class="titleFilter">     
                             <h1>Por data</h1>  
-                            <button class="imgButton openCloseBlock" data-block="dateBlock" type="button"><img src="./assets/images/setaCima.svg"/></button>               
+                            <button class="imgButton openCloseBlock openCloseBtnCss" data-block="dateBlock" type="button"></button>               
                         </div>
                         <div id="dateBlock" class="bodyChecklist" style="display:none">
                             <div class="questionChecklist">
@@ -117,7 +109,7 @@ export class RecordPage extends SettingRecord {
                 </section>
                 <div class="buttonMain">
                     <button type="button">FILTRAR</button> 
-                    <button type="button">LIMPAR</button> 
+                    <button type="button" class="clearBtn" data-clear="dataClear">LIMPAR</button> 
                 </div>
             </aside>
 
