85df26761e25a9e47d9282c4898857a1cee75746
bethuel <bethuel.pg@gmail.com>
2022-01-20 16:06:38 -0300

    modificacoes no limpar e fazendo botao filtro

--- Pages/Record/settingRecord.js
+++ Pages/Record/settingRecord.js
@@ -1,34 +1,78 @@
-
+import { getB_id, $, $_all, openModalCheck, closeModalCheck } from "../../Util/compressSyntaxe.js";
 export class SettingRecord {
+    searchOption;
+    searchDateInit;
+    searchDateFinal;
     setting() {
         this.clickPage();
+        this.filterReposrt()
     }
 
     clickPage() {
         document.addEventListener("click", (event) => {
-            // console.log(event.target)
             if (event.target.tagName.toLowerCase() == "button") {
-                this.filterFunction(event.target)
-                this.clearFilter(event.target)
+                this.functionFilter(event.target)
             }
         })
     }
 
-    filterFunction(element) {
-        if (element.className == "imgButton openCloseBlock openCloseBtnCss") {
-            let bodyBlock = document.getElementById(`${element.getAttribute("data-block")}`)
-            bodyBlock.setAttribute('style', bodyBlock.style.display == 'none' ? "display:block" : "display:none")
+    functionFilter(element) {
+        switch (element.getAttribute("data-function")) {
+            case "openCloseFilter":
+                this.openCloseFilter(element);
+                break;
+            case "clearBtn":
+                this.clearFilter()
+                break;
+            default:
+                console.error("data-function")
         }
     }
 
-    clearFilter(element) {
-        if (element.className == "clearBtn") {
-            let btnClear = `${element.getAttribute("data-clear")}`
-            console.log(btnClear)
-            btnClear.addEventListener("click", () => {
-                let option = document.querySelector('');
-                this.uva = $("#dateFinal").value || "";
-            })
+    openCloseFilter(element) {
+        let bodyBlock = document.getElementById(`${element.getAttribute("data-block")}`)
+        if (bodyBlock.style.display == 'none') {
+            bodyBlock.setAttribute('style', "display:block")
+            element.style.backgroundImage = "url('./assets/images/down.svg')";
+        } else {
+            bodyBlock.setAttribute('style', "display:none")
+            element.style.backgroundImage = "url('./assets/images/up.svg')";
         }
     }
-}
+
+    clearFilter() {
+        this.clearDate()
+        this.clearOptions()
+    }
+
+    clearOptions() {
+        const test = document.querySelectorAll(".sel")
+        test.forEach(options => {
+            options.options[0].selected = true
+        });
+    }
+
+    clearDate() {
+        const data = document.querySelectorAll(".date")
+        data.forEach(date => {
+            date.value = ""
+        })
+    }
+
+    filterReposrt() {
+        const valueDate = document.querySelectorAll(".date")
+
+
+
+        document.addEventListener("click", (event) => {
+            if (event.target.tagName.toLowerCase() == "input") {
+                valueDate.forEach(datas => {
+                    console.log(datas)
+                })
+            }
+
+
+        })
+
+    }
+} 
