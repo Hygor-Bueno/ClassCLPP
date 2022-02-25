e6b618eb11687c03f0fdaee2e6037b4322dcb9d6
José Ricardo <Jos.Ricardo@PegPese.com>
2022-02-25 16:45:37 -0300

    chengeMiniGraphic in Process

--- Pages/Record/settingRecord.js
+++ Pages/Record/settingRecord.js
@@ -95,11 +95,11 @@ export class SettingRecord {
         this.recordObject.setFilters(this.lockInfo())
         this.validationDate()
         let returnReq = await this.recordObject.returnGet(this.recordObject.getParamsForFilters())
+        console.log(returnReq)
         this.reqFiltred = this.recordObject.separateChecklist(returnReq)
         this.recordObject.setPoint(this.recordObject.generalGraphic(this.reqFiltred)[1][1])
         this.populaShopGraphic(returnReq)
-        this.reqFiltred= this.recordObject.separateChecklist(returnReq)
-        this.populaCheckGraphic(returnReq,   this.reqFiltred)
+        this.populaCheckGraphic(returnReq, this.reqFiltred)
         this.clearFilter()
     }
 
@@ -122,7 +122,7 @@ export class SettingRecord {
         getB_id("corpoRecord").onchange = (e) => {
             let getTypeId = e.target[e.target.selectedIndex].getAttribute("id")
             this.changeChartType(getTypeId)
-            this.pressBtnFilter();
+            //this.pressBtnFilter();
         }
     }
     populaShopGraphic(returnReq) {
@@ -276,14 +276,12 @@ export class SettingRecord {
     }
 
     chengeMiniGraphic(id_unity){
-        console.log(id_unity,this.reqFiltred)
         let firstUnity=[];
-        this.reqFiltred.forEach((array) => console.log(array))
+        this.reqFiltred.forEach((array) => array.forEach((value) => {if(value.id_shop == id_unity) firstUnity.push(value)}))
+        console.log(this.recordObject.generalGraphic([firstUnity]))
     }
 
-    closeGraphic() {
-        // getB_id('mainGraphic').getContext('2d').clearRect(0, 0, getB_id('mainGraphic').width, getB_id('mainGraphic').height)
-
+    closeGraphicGeneral() {
         this.recordObject.clppGraphich.graphicRecord && this.recordObject.clppGraphich.graphicRecord.destroy();
     }
     closeMiniGraphic() {     
